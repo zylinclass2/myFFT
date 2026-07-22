da816d0dc6d057b9bb5306e0b31780e22753dd30
zylinclass2 <zylinclass@gmail.com>
2026-07-22 13:52:10 +0800

    First upload

---
+++ My_implement_FFT/testFFT/testFFT.cpp
@@ -0,0 +1,282 @@
+// testFFT.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+//
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define uint16_t    int
+#define uint8_t     int
+#define TRUE        1
+#define FALSE       0
+#define BOOL        short
+
+#define DataAmount ((double)/*1000*/256)
+#define SampleRate ((double)/*500*/64)
+
+#define ForTra      0x00//Forward transform
+#define InvTra      0x01//Inverse transform
+#ifndef PI
+#define PI          3.1415926
+#endif
+
+struct complex_t
+{
+double Re;
+double Im;
+};
+
+void nop()
+{
+	return;
+}
+
+double GetMag(complex_t data,uint16_t DataSize)
+{
+return 2*sqrt(data.Re*data.Re+data.Im*data.Im)/DataSize;
+}
+
+double GetPha(complex_t data)
+{
+return atan2(data.Im,data.Re);
+}
+
+void fftDataReverse(complex_t *fftArry,uint16_t DataSize,uint16_t *len)
+{
+    //uint16_t RevArry[DataSize];
+	uint16_t* RevArry=(uint16_t*)malloc(DataSize*sizeof(uint16_t));
+    complex_t Temp;
+    uint16_t i=0;
+    *len = 0;
+    uint16_t temp_size = DataSize;
+    while (temp_size >>= 1) (*len)++;
+    
+    RevArry[0] = 0;
+    for (i = 1; i < DataSize; i++) {
+        RevArry[i] = (RevArry[i>>1] >> 1) | ((i & 1) << (*len - 1));
+    }
+    
+    for (i = 0; i < DataSize; i++) {
+        if (i < RevArry[i]) {
+            Temp = fftArry[i];
+            fftArry[i] = fftArry[RevArry[i]];
+            fftArry[RevArry[i]] = Temp;
+        }
+    }
+
+free(RevArry);
+}
+
+complex_t MultiComplex(complex_t a,complex_t b)
+{
+complex_t temp;
+temp.Re=a.Re*b.Re-a.Im*b.Im;
+temp.Im=a.Re*b.Im+a.Im*b.Re;
+return temp;
+}
+
+complex_t AddComplex(complex_t a,complex_t b)
+{
+a.Re+=b.Re;
+a.Im+=b.Im;
+return a;
+}
+
+complex_t SubComplex(complex_t a,complex_t b)
+{
+a.Re-=b.Re;
+a.Im-=b.Im;
+return a;
+}
+
+complex_t ConjComplex(complex_t a)
+{
+a.Im=-a.Im;
+return a;
+}
+
+void fft(complex_t *fftArry,uint16_t DataSize,uint8_t TanMode)
+{
+    uint16_t len=0;
+    fftDataReverse(fftArry,DataSize,&len);
+    uint16_t i,j,k;
+    for (i = 1; i <= len; i++)
+	{
+        uint16_t m = 1 << i;
+        complex_t wn;
+        wn.Re = cos(2*PI/m);
+        wn.Im = (TanMode == ForTra) ? -sin(2*PI/m) : +sin(2*PI/m);
+        
+        for (j = 0; j < DataSize; j += m) {
+            complex_t w = {1.0f, 0.0f};
+            for (k = 0; k < m/2; k++) {
+                complex_t Temp1 = MultiComplex(w, fftArry[j+k+m/2]);
+                complex_t Temp2 = fftArry[j+k];
+                
+                fftArry[j+k] = AddComplex(Temp2, Temp1);
+                fftArry[j+k+m/2] = SubComplex(Temp2, Temp1);
+                
+                w = MultiComplex(w, wn);
+            }
+        }
+    }
+    
+    if (TanMode == InvTra) {
+        for (i = 0; i < DataSize; i++) {
+            fftArry[i].Re /= DataSize;
+            fftArry[i].Im /= DataSize;
+        }
+    }
+}
+
+int IsPowerOfTwo(int N)
+{
+double t1=log(N)/log(2);
+int t2=log(N)/log(2);
+if(t1!=(double)t2)return FALSE;
+return TRUE;
+}
+
+int NextPow2(int N)
+{
+double t1=log(N)/log(2);
+int t2=log(N)/log(2);
+if(t1!=(double)t2)return pow(2,t2+1);
+return pow(2,t2);
+}
+
+void bluestein_fft(complex_t *x,int N,uint8_t mode)
+{
+int i=0;
+int M=NextPow2(2*N-1);
+double angle=0;
+double cs=0;
+double sn=0;
+
+complex_t *a=(complex_t*)calloc(M,sizeof(complex_t));
+complex_t *b=(complex_t*)calloc(M,sizeof(complex_t));
+
+if(mode==InvTra)
+{
+for(i=0;i<N;i++)
+x[i]=ConjComplex(x[i]);
+}
+
+for(i=0;i<N;i++)
+{
+angle=PI*i*i/N;
+
+cs=cos(angle);
+sn=sin(angle);
+
+a[i].Re=x[i].Re*cs+x[i].Im*sn;
+a[i].Im=x[i].Im*cs-x[i].Re*sn;
+}//for
+
+for(i=0;i<N;i++)
+{
+angle=PI*i*i/N;
+b[i].Re=cos(angle);
+b[i].Im=sin(angle);
+
+if(i!=0)
+b[M-i]=b[i];
+}
+
+fft(a,M,ForTra);
+fft(b,M,ForTra);
+
+for(i=0;i<M;i++)
+{
+a[i]=MultiComplex(a[i],b[i]);
+}
+
+fft(a,M,InvTra);
+
+for(i=0;i<N;i++)
+{
+angle=PI*i*i/N;
+
+cs=cos(angle);
+sn=sin(angle);
+
+x[i].Re=a[i].Re*cs+a[i].Im*sn;
+x[i].Im=a[i].Im*cs-a[i].Re*sn;
+}
+
+if(mode==InvTra)
+{
+for(i=0;i<N;i++)
+{
+x[i]=ConjComplex(x[i]);
+x[i].Re/=N;
+x[i].Im/=N;
+}
+}
+
+free(a);
+free(b);
+//free(c);
+}
+
+BOOL fft_any(complex_t *x,int N,uint8_t mode)
+{
+if(IsPowerOfTwo(N))
+{
+fft(x,N,mode);
+return TRUE;
+}else
+{
+bluestein_fft(x,N,mode);
+return FALSE;
+}
+}
+
+int main()
+{
+int i=0;
+complex_t* Data=(complex_t*)malloc(DataAmount*sizeof(complex_t));
+double Freq=6;
+double Freq2=10;
+double Freq3=25;
+double Freq4=28.5;
+
+for(i=0;i<DataAmount;i++)
+{
+Data[i].Re=5.3*sin(2*PI*Freq/SampleRate*i)+6.7*sin(2*PI*Freq2/SampleRate*i)+4.3*sin(2*PI*Freq3/SampleRate*i)+9.5*sin(2*PI*Freq4/SampleRate*i)+10;
+Data[i].Im=0;
+}
+
+if(!fft_any(Data,DataAmount,ForTra))goto BluesteinExecution;
+
+printf("=====FFT正变换结果=====\n");
+for(i=0;i<DataAmount;i=i+1)
+{
+if(i==0)
+{
+printf("频点%8.3fHz:幅度=%-7.3f 相位=%-7.3f rad\n",(double)i*SampleRate/DataAmount,GetMag(Data[i],DataAmount)/2,GetPha(Data[i]));
+continue;
+}
+printf("频点%8.3fHz:幅度=%-7.3f 相位=%-7.3f rad\n",(double)i*SampleRate/DataAmount,GetMag(Data[i],DataAmount),GetPha(Data[i]));
+}
+goto ExitCall;
+
+BluesteinExecution:
+printf("=====FFT正变换结果=====\n");
+for(i=0;i<DataAmount;i=i+1)
+{
+if(i==0){
+printf("频点%8.3fHz:幅度=%-7.3f 相位=%-7.3f rad\n",(double)i*SampleRate/DataAmount,GetMag(Data[i],DataAmount)/2,GetPha(Data[i]));
+continue;
+}
+printf("频点%8.3fHz:幅度=%-7.3f 相位=%-7.3f rad\n",(double)i*SampleRate/DataAmount,GetMag(Data[i],DataAmount),GetPha(Data[i]));
+}
+
+ExitCall:
+free(Data);
+return 0;
+}
---
+++ SineWaveFFT/SineWave.c
@@ -0,0 +1,92 @@
+#include "mex.h"
+#include <math.h>
+#include <string.h>
+
+#define LENGTH ((double)2000)
+#define Fs ((double)1000)
+
+void MATLABMain(int nlhs,mxArray* plhs[],int nrhs,const mxArray* prhs[])
+{
+	int i=0;
+	char temppath[100];
+	mxArray* temp[3];
+	mxArray* lefttemp[3];
+	mxArray* mydata[4];//Definitions
+	
+	memset(temp,0,sizeof(temp));
+	memset(lefttemp,0,sizeof(lefttemp));
+	memset(temppath,0,sizeof(temppath));
+	memset(mydata,0,sizeof(mydata));//Initializations
+	
+	mexCallMATLAB(1,lefttemp,0,NULL,"cd");
+	mxGetString(lefttemp[0],temppath,100);
+	mxDestroyArray(lefttemp[0]);
+	
+	mydata[0]=mxCreateDoubleMatrix(1,LENGTH,mxREAL);
+	mydata[1]=mxCreateDoubleMatrix(1,LENGTH,mxREAL);
+	mydata[2]=mxCreateDoubleMatrix(1,LENGTH,mxREAL);
+	mydata[3]=mxCreateDoubleMatrix(1,LENGTH,mxREAL);
+	for(i=0;i<LENGTH;i++)
+	{
+		mxGetPr(mydata[0])[i]=1/Fs*i;//T=1/f
+		mxGetPr(mydata[1])[i]=0.7*sin(mxGetPr(mydata[0])[i]*2*3.1416*50)+2.83*sin(mxGetPr(mydata[0])[i]*2*3.1416*70);
+		//Generate a discrete signal
+	}
+    
+    mexCallMATLAB(1,&mydata[3],1,&mydata[1],"fft");
+	for(i=0;i<LENGTH;i++)
+	{
+		//mxGetPr(mydata[3])[i]=abs(mxGetPr(mydata[3])[i]);
+		mxGetPr(mydata[2])[i]=i/LENGTH*Fs;
+		
+		if(i>=(LENGTH/2)){
+		mxGetPr(mydata[3])[i]=0;
+		}
+		//The fft result will be mirrored at half of Nyquist frequency.
+		//Another half of frequency map will not be showed.
+	}
+    
+	temp[0]=mxCreateDoubleScalar(2);
+	temp[1]=mxCreateDoubleScalar(1);
+	temp[2]=mxCreateDoubleScalar(1);
+	mexCallMATLAB(0,NULL,3,temp,"subplot");
+	mxDestroyArray(temp[2]);
+	mxDestroyArray(temp[1]);
+	mxDestroyArray(temp[0]);
+	
+	temp[0]=mydata[0];
+	temp[1]=mydata[1];
+	mexCallMATLAB(0,NULL,2,temp,"plot");
+	temp[0]=NULL;
+	temp[1]=NULL;
+
+	temp[0]=mxCreateDoubleScalar(2);
+	temp[1]=mxCreateDoubleScalar(1);
+	temp[2]=mxCreateDoubleScalar(2);
+	mexCallMATLAB(0,NULL,3,temp,"subplot");
+	mxDestroyArray(temp[2]);
+	mxDestroyArray(temp[1]);
+	mxDestroyArray(temp[0]);
+
+	temp[0]=mydata[2];
+	temp[1]=mydata[3];
+	mexCallMATLAB(0,NULL,2,temp,"plot");
+	temp[0]=NULL;
+	temp[1]=NULL;
+
+	mxDestroyArray(mydata[0]);
+	mxDestroyArray(mydata[1]);
+	mxDestroyArray(mydata[2]);
+	mxDestroyArray(mydata[3]);
+	//Add your code here.
+
+	/*CD to previous folder before exit*/
+	temp[0]=mxCreateString(temppath);
+	mexCallMATLAB(0,NULL,1,temp,"cd");
+	mxDestroyArray(temp[0]);
+}
+
+void mexFunction(int nlhs,mxArray* plhs[],int nrhs,const mxArray* prhs[])
+{
+	return MATLABMain(nlhs,plhs,nrhs,prhs);
+}
